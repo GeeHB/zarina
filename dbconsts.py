9801a6812961fc9e3d103bb55007a7e0cdf7b27b
Jérôme Henry-Barnaudière - JHB <henry-barnaudiere.j@allier.fr>
2025-08-05 16:41:25 +0200

    Ajout du shéma

--- dbconsts.py
+++ dbconsts.py
@@ -17,6 +17,33 @@ DB_PORT = 3306
 DB_USER = "localAdmin"
 DB_PWD = "Scala_0392"
 
+#
+# Schéma
+#
+
+DB_USER_TABLE   = "user"
+DB_USER_ID      = "idUser"
+DB_USER_PROFIL  = "idProfil"
+DB_USER_FNAME   = "prenom"
+DB_USER_FNAME   = "name"
+
+DB_EVENT_TABLE  = "event"
+DB_EVENT_ID     = "idEvent"
+DB_EVENT_USER   = "idAgent"
+DB_EVENT_TYPE   = "idType"
+DB_EVENT_TITLE  = "liebelle"
+DB_EVENT_START  = "debut"
+DB_EVENT_LAST   = "duree"
+DB_EVENT_STATUS = "etat"
+
+EVENT_TYPE_REST = 0
+EVENT_TYPE_WORK = 1
+EVENT_TYPE_MEAL_TIME = 2
+
+DB_TYPE_TABLE   = "type"
+DB_TYPE_ID      = "idType"
+DB_TYPE_TITLE   = "libelle"
+
 # Messages d'erreur
 #
 DB_ERROR_ = "hello"
